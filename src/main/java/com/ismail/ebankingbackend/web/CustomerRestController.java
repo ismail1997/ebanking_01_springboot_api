3c7dfd968b3dadec995fc76aa779ca8b837e094f
ismail1997 <bouaddi1997@gmail.com>
2024-02-15 15:39:00 +0100

    configure swagger , and expose login path

--- src/main/java/com/ismail/ebankingbackend/web/CustomerRestController.java
+++ src/main/java/com/ismail/ebankingbackend/web/CustomerRestController.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -23,33 +24,39 @@ public class CustomerRestController {
 
 
     @GetMapping
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ResponseEntity<List<CustomerDTO>> getAll(){
         return ResponseEntity.ok().body(this.bankAccountService.listCustomers());
     }
 
     @GetMapping("/search")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ResponseEntity<List<CustomerDTO>> getAll(@RequestParam(name = "keyword",defaultValue = "") String keyword){
         return ResponseEntity.ok().body(this.bankAccountService.searchCustomers("%"+keyword+"%"));
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable("id") Long customerID) throws CustomerNotFoundException {
         return ResponseEntity.ok().body(this.bankAccountService.getCustomerByID(customerID));
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO)
     {
         return ResponseEntity.created(URI.create("/customers/customerID")).body(bankAccountService.saveCustomer(customerDTO));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable("id") Long id, @RequestBody CustomerDTO customerDTO)
     {
         customerDTO.setId(id);
         return ResponseEntity.ok().body(bankAccountService.updateCustomer(customerDTO));
     }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public ResponseEntity<Void> deleteCustomer(@PathVariable("id")  Long id)
     {
         this.bankAccountService.deleteCustomer(id);
@@ -57,6 +64,7 @@ public class CustomerRestController {
     }
 
     @GetMapping("/{id}/accounts")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ResponseEntity<List<BankAccountDTO>>getAccountsOfCustomer(@PathVariable("id") Long id){
         return ResponseEntity.ok().body(this.bankAccountService.listBankAccountOfCustomer(id));
     }
